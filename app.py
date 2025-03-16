e14494bd9548f34ed5b1d7329d9b65868a771fca
Faniswa1 <Faniswamhlanga@gmail.com>
2025-03-16 21:54:22 +0200

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import logging
 import stripe
 from flask import Flask
-from flask_sqlalchemy import SQLALCHEMY
+from flask_sqlalchemy import sqlalchemy
 from flask_login import LoginManager
 from models import db, User, UnclaimedFund
 from routes import create_routes
