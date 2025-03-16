9d1d8bc03841051dbe7f390aa5b3f8c911b96869
Faniswa1 <Faniswamhlanga@gmail.com>
2025-03-16 22:00:15 +0200

    config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SESSION_SECRET", "dev_secret_key_123")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_recycle": 300,
