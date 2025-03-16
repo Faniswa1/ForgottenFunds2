78a8f8156021d7daf91df298b9ed121704c3f46d
Faniswa1 <Faniswamhlanga@gmail.com>
2025-03-16 23:39:07 +0200

    config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SESSION_SECRET", "dev_secret_key_123")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_recycle": 300,
-        "pool_pre_ping": True,
-    }
+
+# ...existing code...
 
