ce8c51297bd55a8b151977b7587f550aad92590f
Faniswa1 <Faniswamhlanga@gmail.com>
2025-03-16 22:49:23 +0200

    config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SESSION_SECRET", "dev_secret_key_123")
-    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///default.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
