1ec2e2fe8833e99a9bd0800429264783a67a8064
Faniswa1 <Faniswamhlanga@gmail.com>
2025-03-17 00:39:41 +0200

    config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     # Fix DATABASE_URL handling
     DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///site.db")  # Default for local dev
     if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace(postgresql://root:rF5BQJQMVaeBx0MMMcg8xFdqF249ldRr@dpg-cvbkn7ofnakc73dm2edg-a/unclaimed_funds, 1)  # Render fix
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)  # Render fix
 
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -15,6 +15,3 @@ class Config:
         "pool_pre_ping": True,
     }
     DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
-
-
-
